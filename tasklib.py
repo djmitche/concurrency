78112a9a07b9ceeec9aec9d0ad9e0592f703dc9d
Dustin J. Mitchell <dustin@mozilla.com>
2012-03-19 13:42:21 -0500

    add events to enable waiting for stop and start

--- tasklib.py
+++ tasklib.py
@@ -2,18 +2,23 @@
 
 import threading
 import logging
+import time
 import sys
 
 class Task:
     def __init__(self,name="Task"):
         self.name = name
         self.state = "INIT"
+        self.start_evt = threading.Event()
+        self.stop_evt = threading.Event()
 
     # Task start
     def start(self):
+        self.start_evt.clear()
         thr = threading.Thread(target=self.bootstrap)
         thr.daemon = True
         thr.start()
+        self.start_evt.wait()
 
     # Task bootstrap
     def bootstrap(self):
@@ -22,17 +27,22 @@ class Task:
         self.exc_info = None
         self.state = "RUNNING"
         self.log.info("Task starting")
+        self.start_evt.set()
         try:
             self.run()
         except Exception:
             self.exc_info = sys.exc_info()
             self.log.error("Crashed", exc_info=True)
+        self.stop_evt.set()
         self.log.info("Exit")
         self.state = "EXIT"
 
     # Task stop
-    def stop(self):
+    def stop(self, wait=False):
         self.runnable = False
+        if wait:
+            self.stop_evt.wait()
+        self.stop_evt.clear()
 
     # Task finalization
     def finalize(self):
